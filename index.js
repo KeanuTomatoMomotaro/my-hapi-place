a162364ae6391af507f1632e9d90fa66ef72acf6
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2017-12-19 14:58:33 +0700

    jimp image manipulation

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 'use strict';
 var fs = require('fs');
 const Hapi = require('hapi');
-var mysql = require('mysql');
 var Jimp = require('jimp');
-var moment = require('moment');
-var bcrypt = require('bcrypt');
 
 //server stuff
 const server = Hapi.server({
     host: 'localhost',
     port: 8000,
-});
-
-//db mysql config, change later before merge
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'leantera_production_web_root'
-});
-//initiate connection to database
-connection.connect(function(err){
-    if(err){
-        console.log("failed to connect to database");
-        connection.end();
-    }
 });
 
 server.route([
@@ -35,7 +17,36 @@ server.route([
             return "Hello Motorola";
         }
     },
-])
+    {
+        method: 'POST',
+        path: '/create-black-n-white',
+        config: {
+            payload: {
+                maxBytes: 209715200,
+                output:'stream',
+                parse: true
+            },
+            handler: function (request, reply) {
+                request.payload['image'].pipe(fs.createWriteStream("images/originals/originalImage.jpg"));
+                Jimp.read("images/originals/originalImage.jpg", function (err, image){
+                    if(err) throw err
+                    image.resize(100,100)
+                    .quality(60)
+                    .greyscale()
+                    .write('images/blackAndWhite/blackAndWhite.jpg');
+                })
+                //success
+                const response = reply.response({
+                    message: "Uploaded and Altered Image"
+                });
+                //response type app / json
+                response.type('application/json');
+                
+                return response;
+            }
+        }
+    }
+]);
 
 //start server
 async function start() {
